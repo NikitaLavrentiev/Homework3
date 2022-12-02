09a870c25ad9fa8689121f90b8b33fc7c166df82
Никита <lavrentivev.nikita@gmail.com>
2022-12-03 00:33:37 +0300

    4th task in progress...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
     }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte a = 100;
+        short b = 10000;
+        int c = 100000000;
+        long d = 1000000000000;
+        float e = 5,5;
+        double f = 6,70000000000000000000000000000000;
+
+
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+        double a = 27.12;
+        int b = 987678965549;
+        float c = 2,786;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+        byte a = 23;
+        byte b = 27;
+        byte c = 30;
+        int all = (a + b + c);
+        short paper = 480;
+        int limPaper = paper / all;
+        System.out.println("На каждого ученика рассчитано " + limPaper+ " листов бумаги");
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+        byte productivity = 8;
+        byte timeToWork = 1;
+
+
+
 }
+
